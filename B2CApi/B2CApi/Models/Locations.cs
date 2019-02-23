8f6ac569371cd015ccd0318a49a0149625ff2511
Mitya Nenilin <mnenilin@systemorph.com>
2019-02-23 21:34:06 +0300

    Add search controller, models added

---
+++ B2CApi/B2CApi/Models/Locations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace B2CApi.Models
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Locations
+    {
+        [JsonProperty]
+        public IList<Location> LocationsList { get; set; }
+
+        public Locations()
+        {
+            LocationsList = new List<Location>();
+        }
+    }
+}
